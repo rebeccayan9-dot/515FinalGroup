7c50bbcd2e8dc27291a9dcc29687806d08a35de7
rebeccayan9-dot <qianmu@uw.edu>
2026-04-29 16:44:03 -0700

    organized data and run testing, cut the trees to 50, add the new model.

--- train_model.py
+++ train_model.py
@@ -88,7 +88,8 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(
         X, y_enc, test_size=0.2, random_state=42, stratify=y_enc
     )
-    clf = RandomForestClassifier(n_estimators=100, max_depth=10,
+    clf = RandomForestClassifier(n_estimators=50, max_depth=10,
+                                 class_weight='balanced',
                                  random_state=42, n_jobs=-1)
     clf.fit(X_train, y_train)
 
